a5aa1aeb0682a1a41f2d2ef34687fdf9ab117619
Kasaksaxena <kasaksaxena6@gmail.com>
2025-05-10 01:02:08 +0530

    run django project

--- drf/py_client/basic.py
+++ drf/py_client/basic.py
@@ -2,5 +2,10 @@ import requests
 # endpoint is webaddress(url) u call to get specific data from an api
 endpoint= "https://httpbin.org/status/200/"
 endpoint="https://httpbin.org/anything"
+endpoint="http://localhost:8000/"
 get_request=requests.get(endpoint)
-print(get_request.json())
+#print(get_request.json())
+print(get_request.text)
+
+print(get_request.status_code)
+
