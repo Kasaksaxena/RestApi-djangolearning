9b9199918f345dd801a8822a7b4dfd743580ab12
Kasaksaxena <kasaksaxena6@gmail.com>
2025-05-10 01:29:22 +0530

    creating first api view

--- drf/py_client/basic.py
+++ drf/py_client/basic.py
@@ -2,9 +2,9 @@ import requests
 # endpoint is webaddress(url) u call to get specific data from an api
 endpoint= "https://httpbin.org/status/200/"
 endpoint="https://httpbin.org/anything"
-endpoint="http://localhost:8000/"
+endpoint="http://localhost:8000/api"
 get_request=requests.get(endpoint)
-#print(get_request.json())
+print(get_request.json()["message"])
 print(get_request.text)
 
 print(get_request.status_code)
