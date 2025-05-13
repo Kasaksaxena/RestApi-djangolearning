1f5877d997e5195b38bcf7e54bee73a044d96e69
Kasaksaxena <kasaksaxena6@gmail.com>
2025-05-14 02:09:02 +0530

    Django model instance as api response

--- drf/backend/cfehome/api/views.py
+++ drf/backend/cfehome/api/views.py
@@ -1,9 +1,19 @@
 import json
 from django.shortcuts import render
 from django.http import JsonResponse
+from products.models import Product
 # Create your views here.
 def api_home(request,*args, **kwargs):
-    print(request.GET)
+    model_data=Product.objects.all().order_by("?").first()   
+    data={}
+    if model_data: 
+        data['id']=model_data.id
+        data['title']=model_data.title
+        data['content']=model_data.content
+        data['price']=model_data.price
+
+
+    """print(request.GET)
     body=request.body #byte string of json data
     data={}
     try:
@@ -15,5 +25,5 @@ def api_home(request,*args, **kwargs):
     print(request.headers)
     json.dumps(dict(request.headers))
     data["content_type"]=request.content_type
-    
+    """
     return JsonResponse(data)
--- drf/py_client/basic.py
+++ drf/py_client/basic.py
@@ -1,12 +1,13 @@
 import requests
 # endpoint is webaddress(url) u call to get specific data from an api
-endpoint= "https://httpbin.org/status/200/"
-endpoint="https://httpbin.org/anything"
+#endpoint= "https://httpbin.org/status/200/"
+#endpoint="https://httpbin.org/anything"
 endpoint="http://localhost:8000/api/"
-get_request=requests.get(endpoint, params={"abc": 123}, json={"query":"Hello World!"})
+#get_request=requests.get(endpoint, params={"abc": 123}, json={"query":"Hello World!"})
 #print(get_request.json()["message"])
 #print(get_request.text)
+get_request=requests.get(endpoint)
 print(get_request.json())
 
-print(get_request.status_code)
+#print(get_request.status_code)
 
