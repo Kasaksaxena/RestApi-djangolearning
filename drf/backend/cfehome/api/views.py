a7dc2eff6488a660a2e47ab9b1f4010bddaa01a9
Kasaksaxena <kasaksaxena6@gmail.com>
2025-06-16 12:34:11 +0530

    Restframework views and response

--- drf/backend/cfehome/api/views.py
+++ drf/backend/cfehome/api/views.py
@@ -4,13 +4,24 @@ from django.http import JsonResponse,HttpResponse
 from products.models import Product
 from django.forms.models import model_to_dict
 from django.core.serializers.json import DjangoJSONEncoder
+
+#api_view
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 # Create your views here.
+
+@api_view(["GET","POST"])
 def api_home(request,*args, **kwargs):
+    """ 
+    DRF API VIEW
+    """
+    if request.method=="POST":
+        return Response({"detail": "GET not Allowed"},status=405)
     model_data=Product.objects.all().order_by("?").first()   
     data={}
     if model_data: 
         data=model_to_dict(model_data, fields=['id','title',"price"])
-        return JsonResponse(data)
+        return Response(data)
         # json_data_str=json.dumps(data, cls=DjangoJSONEncoder)
 
 # """print(request.GET)
