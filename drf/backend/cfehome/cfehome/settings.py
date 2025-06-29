6cd949d50dd5d5b69497d393e72d03420fd268d1
Kasaksaxena <kasaksaxena6@gmail.com>
2025-06-29 14:56:05 +0530

    PAGINATION

--- drf/backend/cfehome/cfehome/settings.py
+++ drf/backend/cfehome/cfehome/settings.py
@@ -138,5 +138,9 @@ REST_FRAMEWORK={
     ],
     "DEFAULT_PERMISSION_CLASSES":[
         "rest_framework.permissions.IsAuthenticatedOrReadOnly" #get
-    ]
+    ],
+    "DEFAULT_PAGINATION_CLASS":
+       "rest_framework.pagination.LimitOffsetPagination",
+       "PAGE_SIZE":10 
+    
 }
